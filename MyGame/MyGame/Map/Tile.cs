489211b0f6589e695f0bbca6a1aa02cdafb5ed3d
zonov <jesusdangerous@users.noreply.github.com>
2024-04-15 15:15:18 +0500

    animated map

--- MyGame/MyGame/Map/Tile.cs
+++ MyGame/MyGame/Map/Tile.cs
@@ -11,32 +11,36 @@ namespace MyGame.Map
 {
     public class Tile
     {
-        private const int Width = 16;
-        private const int Height = 16;
+        protected const int Width = 16;
+        protected const int Height = 16;
 
         public int XPos { get; set; }
         public int YPos { get; set; }
         public int ZPos { get; set; }
 
-        public int TextureXPos { get; set; }
-        public int TextureYPos { get; set; }
+        public List<TileFrame> TileFrames { get; set; }
+        public int AnimationSpeed { get; set; }
+        private double _counter;
+        private int _animationIndex;
+
 
         public string TextureName { get; set; }
-        private Texture2D _texture;
+        protected Texture2D _texture;
 
         public Tile()
         {
 
         }
 
-        public Tile(int xPos, int yPos, int zPos, int textureXPos, int  textureYPos, string textureName)
+        public Tile(int xPos, int yPos, int zPos, List<TileFrame> tileFrames, int animationSpeed, string textureName)
         {
             XPos = xPos;
             YPos = yPos;
             ZPos = zPos;
-            TextureXPos = textureXPos;
-            TextureYPos = textureYPos;
+            TileFrames = tileFrames;
+            AnimationSpeed = animationSpeed;
             TextureName = textureName;
+            _animationIndex = 0;
         }
 
         public void LoadContent(ContentManager content)
@@ -46,15 +50,26 @@ namespace MyGame.Map
 
         public void Update(double gameTime)
         {
+            if (TileFrames.Count <= 1)
+                return;
 
+            _counter += gameTime;
+            if (_counter > AnimationSpeed)
+            {
+                _counter = 0;
+                _animationIndex++;
+                if (_animationIndex >= TileFrames.Count)
+                    _animationIndex = 0;
+            }
         }
 
-        public void Draw(SpriteBatch spriteBatch)
+        public virtual void Draw(SpriteBatch spriteBatch)
         {
             spriteBatch
                 .Draw(_texture, 
                 new Rectangle(XPos * Width, YPos * Height, Width, Height), 
-                new Rectangle(TextureXPos * Width, TextureYPos * Height, Width, Height), Color.White);
+                new Rectangle(TileFrames[_animationIndex].TextureXPos * (Width - 1), 
+                TileFrames[_animationIndex].TextureYPos * (Height - 1), Width, Height), Color.White);
         }
     }
 }
--- MyGame/MyGame/obj/Debug/net6.0/MyGame.AssemblyInfo.cs
+++ MyGame/MyGame/obj/Debug/net6.0/MyGame.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c540f11a8716a1303a7c7049e7898b00952fd50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebf3c34673a159a0b4431bb5f3b31b363f08ff3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
